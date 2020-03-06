bcce9f5383f69b4aa1712f649a2c9b4da4e0577d
Enginovic <Engins.93@gmail.com>
2020-03-06 14:23:30 +0100

    fill in first letter

---
+++ labday-lingo.js
@@ -0,0 +1,34 @@
+const words = [
+    'linux',
+    'mouse',
+    'timer',
+    'basic',
+];
+
+let lingoGame;
+
+/**
+* Start a new single player game
+*/
+function startSinglePlayerGame() {
+    var playerOptions = document.getElementById("player-options");
+    var lingoLayout = document.getElementById("lingo__layout");
+    playerOptions.style.display = "none";
+    lingoLayout.style.display = "flex";
+
+    lingoGame = new LingoGame(words);
+    let round = lingoGame.start();
+    fillFirstLetter(round);
+}
+
+function fillFirstLetter(round) {
+    let firstCell = document.querySelector('.cell--first span');
+    firstCell.innerHTML = round.word.charAt(0);
+}
+
+function guessButtonHandler() {
+    // fetch current guess from input field
+    let guessResult = round.guess(guessWord);
+    console.log(guessResult);
+    // C result op grid
+}
